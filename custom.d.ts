dcbae66fdd7951462140ccb6ca60ef3a035fc992
GSS <geisel@gmail.com>
2026-06-24 12:21:25 +0100

    first commit

---
+++ custom.d.ts
@@ -0,0 +1,85 @@
+declare const _vtela: { soap: number, codtela: number, editar: number, excluir: number, incluir: number, especiais:array };
+declare const br: { t: "br" };
+declare const _urlxml: string;
+declare const _w: Window;
+declare const _d: Document;
+declare const ForupGF: {
+	TransferenciaEntreContas: any
+	Financeiro: any
+	Movimentacao: any
+	Transacao: any
+	Cobranca: any
+	Titulo: any
+	ContaConciliacao: {
+		pesquisaSacado:{}
+		tabelaCobrancas:HTMLTableSectionElement
+	}
+};
+declare const sp: '&nbsp;';
+declare const sp2: '&nbsp;&nbsp;';
+declare const sp4: '&nbsp;&nbsp;&nbsp;&nbsp;';
+declare const tinymce:any;
+
+declare const financeiro: {};
+
+declare function _get(op: string | HTMLElement): HTMLElement;
+declare function agora(): number;
+declare function required(): void;
+declare function iconeVideo(url: string): HTMLElement;
+declare function comparaData(data1: any, data2: any): number;
+declare function data2sys(valor: string | number, formato?: string): string | number;
+declare function geraTela(op: object): HTMLElement;
+declare function popUp(op: object): HTMLElement;
+declare function remove(element: string | HTMLElement);
+declare function includeOnce(...parameters: any[]);
+declare function ApiConnect(codtela: number);
+
+declare class ConexaoSOAP {
+	constructor(codtela: number, servico: string)
+	addParametro(id: string, value: any)
+	getObjeto()
+}
+
+declare class Icone {
+	constructor(...parameters: any[])
+	static getIcone(icone: string | object, attrib?: object)
+}
+
+declare class PopUpMenu {
+	constructor(...parameters: any[])
+	show()
+}
+
+declare class PesquisaExt {
+	recuperaPagina: any
+	tela: number
+	funcao: string
+	tamanhoPagina: number
+	modo: string
+	geraTabela(itens:array,op:{})
+	recordSets:array
+}
+
+declare class ApiConnect {
+	constructor(codtela: number)
+}
+
+interface Window {
+	financeiro: any
+	ForupGF: any
+}
+
+declare class FileUpload {
+	constructor(...parameters: any[])
+	getFiles(): any[]
+}
+
+interface ChildNode {
+	insertRowExt(index?: any): HTMLTableRowElement;
+	appendChildExt(op: object | string, atrib?: object): HTMLElement | any[];
+}
+
+interface HTMLElement {
+	value: any;
+	showModal(...parameters: any[]);
+}
